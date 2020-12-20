13a99e5f8270ce1a8c088a7b27f7c248fae29cc5
Chris Segura <chrisseg@microsoft.com>
2020-12-20 12:38:45 -0800

    upload file maybe

--- pingtest/UnitTest1.cs
+++ pingtest/UnitTest1.cs
@@ -1,4 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
 using asciitestingNS;
 
 namespace wopr
@@ -6,6 +8,29 @@ namespace wopr
     [TestClass]
     public class connectioncheck
     {
+        
+        TestContext TestContext { get; set; }
+        /*
+        [TestInitialize]
+        public void Test_Assesembly_Initialize(TestContext context)
+        {
+            FileStream ostrm;
+            StreamWriter writer;
+            TextWriter oldOut = Console.Out;
+            string _fileName = "./TestResult_" + DateTime.Now.ToString("yyyyMMddTHHmm") + ".txt";
+            try
+            {
+                ostrm = new FileStream(_fileName, FileMode.OpenOrCreate, FileAccess.Write);
+                writer = new StreamWriter(ostrm);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Cannot open file for writing");
+                Console.WriteLine(e.Message);
+                return;
+            }
+        }
+        */
         [TestMethod]
         public void Ping_Known_Good_Address()
         {
@@ -14,10 +39,34 @@ namespace wopr
             bool expectedResult = true;
             bool returnedResult;
 
+            //test file attachment
+            FileStream ostrm;
+            StreamWriter writer;
+            TextWriter oldOut = Console.Out;
+            string _fileName = "./TestResult_" + DateTime.Now.ToString("yyyyMMddTHHmm") + ".txt";
+            try
+            {
+                ostrm = new FileStream(_fileName, FileMode.OpenOrCreate, FileAccess.Write);
+                writer = new StreamWriter(ostrm);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Cannot open file for writing");
+                Console.WriteLine(e.Message);
+                return;
+            }
+            Console.SetOut(writer);
+            Console.WriteLine("Test result for : {0}", TestContext.TestName + DateTime.Now.ToString("yyyyMMdd"));
+            Console.WriteLine("Everything is awesome");
+            Console.SetOut(oldOut);
+            writer.Close();
+            ostrm.Close();
+            TestContext.AddResultFile(_fileName);
+
             PingSpectrumFiveG sp = new PingSpectrumFiveG();
             returnedResult = sp.PingServer(host, port);
 
-            Assert.IsTrue(returnedResult == expectedResult, "ping succeeded");
+            Assert.IsTrue(returnedResult == expectedResult, "ping failed");
 
         }
 
@@ -33,7 +82,15 @@ namespace wopr
             returnedResult = sp.PingServer(host, port);
 
             //should throw exception... 
-            Assert.IsTrue(returnedResult == expectedResult, "ping succeeded");
+            Assert.IsTrue(returnedResult == expectedResult, "ping wrong address");
+        }
+
+        [TestCleanup]
+        public void Test_Assembly_Cleanup(TestContext context)
+        {
+            //Console.SetOut(oldOut);
+            //writer.Close();
+            //ostrm.Close();
         }
     }
 }
